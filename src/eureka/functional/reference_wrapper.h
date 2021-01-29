10522b2879d73b6b1f824d55b748271ec43e25c1
pascal.mingjie@gmail.com <pascal.mingjie@gmail.com>
2021-01-29 14:45:41 +0800

    reference_wrapper.h comments

--- src/eureka/functional/reference_wrapper.h
+++ src/eureka/functional/reference_wrapper.h
@@ -26,7 +26,7 @@ class reference_wrapper {
   template<typename Param>
   using reference_wrapper_enabler = decltype(
   _impl::reference_wrap<Arg>(declared_value<Param>()),
-	  enable_if_t < !is_same_v < reference_wrapper, remove_const_volatile_reference < Param >>, placeholder_t > ());
+	  enable_if_t<!is_same_v<reference_wrapper, remove_const_volatile_reference<Param >>, placeholder_t>());
 
  public:
   using type = Arg;
@@ -74,23 +74,59 @@ class reference_wrapper {
 template<typename Arg>
 reference_wrapper(Arg &)-> reference_wrapper<Arg>;
 
+/**
+ * \brief type traits for reference_wrapper
+ * \tparam Arg
+ */
 template<typename Arg>
 struct is_reference_wrapper : false_t {};
 template<typename Arg>
 struct is_reference_wrapper<reference_wrapper<Arg>> : true_t {};
 eureka_value_helper_macro(is_reference_wrapper);
 
+/**
+ * \brief make a reference_wrapper from an lvalue reference
+ * \tparam Arg
+ * \param arg
+ * \return
+ */
 template<typename Arg>
 inline reference_wrapper<Arg> ref(Arg &arg) noexcept { return reference_wrapper<Arg>(arg); }
+/**
+ * \brief make a reference_wrapper from a const lvalue reference
+ * \tparam Arg
+ * \param arg
+ * \return
+ */
 template<typename Arg>
 inline reference_wrapper<const Arg> cref(const Arg &arg) noexcept { return reference_wrapper<const Arg>(arg); }
+/**
+ * \brief make a reference_wrapper from an rvalue reference
+ * \tparam Arg
+ */
 template<typename Arg>
 void ref(const Arg &&) = delete;
+/**
+ * \brief make a reference_wrapper from a const rvalue reference
+ * \tparam Arg
+ */
 template<typename Arg>
 void cref(const Arg &&) = delete;
+/**
+ * \brief prevent reference_wrappers to degenerate when making new reference wrappers
+ * \tparam Arg
+ * \param arg
+ * \return
+ */
 template<typename Arg>
 inline reference_wrapper<Arg>
 ref(reference_wrapper<Arg> arg) noexcept { return arg; }
+/**
+ * \brief prevent reference_wrappers to degenerate when making new reference wrappers
+ * \tparam Arg
+ * \param arg
+ * \return
+ */
 template<typename Arg>
 inline reference_wrapper<const Arg>
 cref(reference_wrapper<Arg> arg) noexcept { return {arg.get()}; }
